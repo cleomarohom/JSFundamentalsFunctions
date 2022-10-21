385081b820f84305ded25d4c6146bd70ccf5700e
Cleo Khaye Marie Marohom <cleomarohom@gmail.com>
2022-10-21 03:20:46 -1200

    Add files via upload

---
+++ act3.js
@@ -0,0 +1,9 @@
+function alphabet_order(str)
+  {
+    
+return str.split('').sort().join('');
+  }
+console.log(alphabet_order("cleo"));
+console.log(alphabet_order("khaye"));
+console.log(alphabet_order("marie"));
+console.log(alphabet_order("marohom"));
